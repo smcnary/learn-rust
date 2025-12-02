162d645ad89d751d1c4085e53382a8c168fc7b5e
Sean McNary <seanmcnary@gmail.com>
2025-12-02 13:58:27 -0600

    Add tests for List operations

--- src/first.rs
+++ src/first.rs
@@ -13,6 +13,8 @@ struct Node {
     elem: i32,
     next: Link,
 }
+// pub says we want people outside this module to be able to use List
+
 
 impl List {
     pub fn new() -> Self {
@@ -35,6 +37,10 @@ impl List {
             }
         }
     }
+
+
+    
+    
 }
 
 impl Drop for List {
@@ -47,3 +53,35 @@ impl Drop for List {
     }
 }
 
+
+mod test {
+    #[test]
+    fn basics() {
+        use super::List;
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
