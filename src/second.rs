fa8cee0eb7fe30b10caf0b0b5672f0b53efd7105
Sean McNary <seanmcnary@gmail.com>
2025-12-03 15:09:11 -0600

    Add IntoIter implementation and tests

--- src/second.rs
+++ src/second.rs
@@ -6,10 +6,33 @@ pub struct List<T> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
+
+
 struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+// Tuple structs are an alternative form of struct,
+// useful for trivial wrappers around other types.
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+// pub says we want people outside this module to be able to use List
+
 
 impl<T> List<T> {
     pub fn new() -> Self {
@@ -44,6 +67,10 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
+
+
+    
+    
 }
 
 impl<T> Drop for List<T> {
@@ -55,4 +82,34 @@ impl<T> Drop for List<T> {
         }
     }
 }
+#[test]
+fn peek() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+
+    list.peek_mut().map(|value| {
+        *value = 42
+    });
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
+}
+
 
